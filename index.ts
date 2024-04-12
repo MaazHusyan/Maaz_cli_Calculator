47411255d545e3c12ebff55ef51be4ee5c5d70bf
Maaz Husyan <159529283+MaazHusyan@users.noreply.github.com>
2024-04-12 11:27:38 +0500

    This is a TypeScript Calculator

---
+++ index.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+  { message: "Enter your First Number", type: "number", name: "firstNum" },
+  { message: "Enter your Second Number", type: "number", name: "secondNum" },
+  {
+    message: "Please Select Your Operator",
+    type: "list",
+    name: "operator",
+    choices: ["Add", "Subtract", "Multiply", "Divide"],
+  },
+]);
+// Conditional Statement
+if (answer.operator === "Add") {
+  console.log(answer.firstNum + answer.secondNum);
+} else if (answer.operator === "Subtract") {
+  console.log(answer.firstNum - answer.secondNum);
+} else if (answer.operator === "Multiply") {
+  console.log(answer.firstNum * answer.secondNum);
+} else if (answer.operator === "Divide") {
+  console.log(answer.firstNum / answer.secondNum);
+} else {
+  console.log("Invalid Operation");
+}
+
